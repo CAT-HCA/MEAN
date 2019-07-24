9948d5ddf7b28ba265c8225bf511769d415dc133
Rob Frenette <RMF.HCA2@gmail.com>
2019-07-24 15:23:50 -0400

    Added MySQL demo using Sequelize pkg.

---
+++ 3_MySQL/03_Sequelize/demo/migrations/20190724180311-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            user_name: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            is_admin: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                defaultValue: 0
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Users');
+    }
+};
