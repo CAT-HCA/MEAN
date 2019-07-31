efe1122a4dbc5642f92580d9ac67bf056821b52b
Rob Frenette <RMF.HCA2@gmail.com>
2019-07-31 08:53:43 -0400

    Added Directives demo.

---
+++ 4_Angular/3_demo_Directives/demo/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mountains';
+
+  mountainName: string = 'Mt. Washington';
+  mountainElevation: string = '6288';
+  mountainDescription: string = 'Mt. Washington (6,288 feet) is the highest peak east of the Mississippi River and north of the Carolinas. The upper part of the mountain has a climate similar to that of northern Labrador and supports a variety of alpine flora and fauna.';
+  currentYear: number = 2019;
+
+  newMountain: string = '';
+
+  addNewMountains: boolean = false;
+  newMountainAdded: boolean = false;
+
+  // declare a method
+  getCurrentYear(): number {
+    return this.currentYear;
+  }
+
+  // executed when the Reset button is clicked
+  onReset(): void {
+    this.mountainName = '';
+    this.mountainElevation = '';
+    this.mountainDescription = '';
+    this.addNewMountains = false;
+    this.newMountainAdded = false;
+  }
+
+  // executed when Add Mountain is clicked
+  onAddMountain(): void {
+    this.newMountain = `${this.mountainName} - ${this.mountainElevation}': ${this.mountainDescription}`;
+    this.addNewMountains = true;
+    this.newMountainAdded = true;
+  }
+
+  getColor(): string {
+    return this.newMountainAdded === true ? '#000080' : '#FF0000'; // navy : red
+  }
+}
